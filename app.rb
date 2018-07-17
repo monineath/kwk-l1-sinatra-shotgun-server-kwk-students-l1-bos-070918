785fd710ad7e184e68f1f999599f7da0ef98f853
monineath <monineathh@gmail.com>
2018-07-17 15:23:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,18 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! I am trying to make a change. This is where shotgun comes in handy. "
   end
+  
+  get '/name/last_name' do
+  "My name is Nick Lennon."
+  end 
 
+  get '/boy_time' do
+  "It is now boy time."
+  end
+  
+  get '/karlie_is_coming_again' do
+  "Hello World!"
+  end
 end
